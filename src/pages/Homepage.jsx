95f18b86861507392bf755ca82ebd5eaf9fc1313
Sasank5716 <sasanklama5716@gmail.com>
2025-03-27 13:28:55 +0545

    Add homepage, MovieCard and css

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import "../css/home.css"
+import MovieCard from '../components/MovieCard'
 
 function Homepage() {
-  return (
-    <div>Homepage</div>
-  )
+  const movies = [
+    {id: 1, title: "Outcast", release_date: "2022-01-01"},
+    { id: 2, title: "Naruto", release_date: "2022-02-01" },
+    { id: 3, title: "Onepiece", release_date: "2022-03-01" },
+];
+
+return (
+  <div className='home'>
+    <div className='movies-grid'>
+      {movies.map((movie)=>(
+        <MovieCard movie={movie} key={movie.id} />
+      ))}
+    </div>
+  </div>
+)
 }
 
 export default Homepage
