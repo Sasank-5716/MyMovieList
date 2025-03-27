38b068eb681981ce7f8a396f4bac717635ce2ff1
Sasank5716 <sasanklama5716@gmail.com>
2025-03-27 13:44:08 +0545

    Add favourites page

--- src/pages/Favourites.jsx
+++ src/pages/Favourites.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 function Favourites() {
   return (
-    <div>Favourites</div>
+    <div className='favourites-empty'>
+      <h2>Nothing here yet...</h2>
+      <p>When you add movies to your favourites, they will appear here.</p>
+
+
+    </div>
   )
 }
 
